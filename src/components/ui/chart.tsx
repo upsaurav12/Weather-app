44f88c2f613fbe379e833e38d0d0f80b82b5f3fe
Saurav Upadhyay <sauravup041103@gmail.com>
2024-10-02 14:33:47 +0530

    add: shadcn charts , cards with dynamic data

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,12 +1,13 @@
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
+/*
 import {
   NameType,
   Payload,
   ValueType,
-} from "recharts/types/component/DefaultTooltipContent"
+} from "recharts/types/co*mponent/DefaultTooltipContent"*/
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const
@@ -72,7 +73,7 @@ ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([_, config]) => config.theme || config.color
+    ([, config]) => config.theme || config.color
   )
 
   if (!colorConfig.length) {
